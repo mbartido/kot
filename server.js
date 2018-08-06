79fc041f25187734d2214fe4ea9878114ce5cfda
Michael Bartido <mbartido@ucsc.edu>
2018-08-06 01:29:45 -0700

    Updated server.js routing

--- server.js
+++ server.js
@@ -19,13 +19,14 @@ mongoose.connect(`mongodb://${apiConfig.username}:${apiConfig.password}@ds211558
                  { useMongoClient: true });
 // Handle the connection
 var db = mongoose.connection;
+mongoose.Promise = global.Promise;         // to avoid deprecation warning
 db.on('error', console.error.bind(console, 'connection error: '));
 db.once('open', function() {
   console.log("DB connection alive.");
 });
 
 // Routing for application---------------------------------------------------
-var Bear =  require('./app/models/player');           // get the model
+var Player =  require('./app/models/player');           // get the model
 var router = express.Router();                        // get instance of router
 // Request middleware
 router.use(function(req, res, next) {
@@ -39,7 +40,79 @@ router.get('/', function(req, res) {
   res.json({ message: 'Welcome to API!' });
 });
 
-// Other routes
+// Routes
+router.route('/players')
+
+  // create a user @ http://localhost:8080/api/users
+  .post(function(req, res) {
+    var player = new Player();         // creates new instance of user
+    // Set initial schema
+    player.name = req.body.name;
+    player.character = req.body.character;
+    player.health = req.body.health;
+    player.points = req.body.points;
+    player.energy = req.body.energy;
+    player.roomID = req.body.roomID;
+    player.inGame = req.body.inGame;
+
+    // Saving the player in DB
+    player.save(function(err) {
+      if (err) res.send(err);
+      res.json({ message: 'Player created!' });
+    });
+  })
+
+  // get all users @ http://localhost:8080/api/users
+  .get(function(req, res) {
+    Player.find(function(err, players) {
+      if (err) res.send(err);
+      res.json(players);
+    });
+  });
+
+// routes that end in /players/:player_id
+router.route('/players/:player_id')
+
+  // get player with id @ http://localhost:8080/api/players/:player_id
+  .get(function(req, res) {
+    Player.findById(req.params.player_id, function(err, player) {
+      if (err) res.send(err);
+      res.json(player);
+    });
+  })
+
+  // update player with id @ http://localhost:8080/api/players/:player_id
+  .put(function(req, res) {
+    // use model to find player we want
+    Player.findById(req.params.player_id, function(err, player) {
+      if (err) res.send(err);
+      // Change info in db to one in request
+      // Change db info if specific request is made
+      if (req.body.name) player.name = req.body.name;
+      if (req.body.character) player.character = req.body.character;
+      if (req.body.health) player.health = req.body.health;
+      if (req.body.points) player.points = req.body.points;
+      if (req.body.energy) player.energy = req.body.energy;
+      if (req.body.roomID) player.roomID = req.body.roomID;
+      if (req.body.inGame) player.inGame = req.body.inGame;
+
+      // Save the player
+      player.save(function(err) {
+        if (err) res.send(err);
+        res.json({ message: 'Player updated!'})
+      })
+    })
+  })
+
+  // delete player with id @ http://localhost:8080/api/players/:player_id
+  .delete(function(req, res) {
+    Player.remove({
+      _id: req.params.bear_id
+    }, function(err, player) {
+      if (err) res.send(err);
+      res.json({message: 'Successfully deleted!' });
+    });
+  });
 
 // Register of routes
 // All routes prefixed by /api
